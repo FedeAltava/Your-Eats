ef1c072cb082f72d861ff6f131f451deecf307cd
Federico <fedealtava1992@gmail.com>
2025-03-14 19:30:29 +0100

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Header.css"
+function Header() {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>order your favorite food here</h2>
+        <p>Choose from a diverse menu featuring a delectable array pf dishes crafted with the
+            finest ingredients and culinary expertise.our mision is to satisy your cravings and
+            elevate your dining experence, one delicius meal at time.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
