6ec687e6d1ae473d2499e881906dbff96de741b0
Federico <fedealtava1992@gmail.com>
2025-03-14 18:49:31 +0100

    navbar css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import "./Navbar.css"
+import { assets } from '../../assets/assets'
+
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'> 
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile-app</li>
+        <li>Contact-us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className="dot"></div>
+        </div>
+        <button>sing in</button>
+      </div>
     </div>
   )
 }
