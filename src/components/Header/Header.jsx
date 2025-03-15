0502a7797478dd58195118627131b804aca420f4
Federico <fedealtava1992@gmail.com>
2025-03-15 18:15:37 +0100

    css header and explore menu component created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className='header'>
       <div className="header-contents">
-        <h2>order your favorite food here</h2>
+        <h2>Order your favorite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array pf dishes crafted with the
             finest ingredients and culinary expertise.our mision is to satisy your cravings and
             elevate your dining experence, one delicius meal at time.
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Home.css"
 import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 export default function Home() {
   return (
     <div>
       <Header />
+      <ExploreMenu />
     </div>
   )
 }
