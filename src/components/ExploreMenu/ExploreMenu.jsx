e0ba6d259742ff7f19756757c80f2fd7707698e5
Federico <fedealtava1992@gmail.com>
2025-03-15 18:45:48 +0100

    explore menu css donde and some logic added (class active)and category pops

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import "./ExploreMenu.css"
 import { menu_list } from '../../assets/assets'
-function ExploreMenu() {
+function ExploreMenu({category, setCategory}) {
+    console.log(category)
   return (
     <div className="explore-menu" id="explore-menu">
         <h1>Explore our menu</h1>
-        <p className='explre-menu-text'>Choose from diverse menu featurnig a delectable array of dishes.Our mission is to satisfy tour craving and elevate your dining experinece,
+        <p className='explore-menu-text'>Choose from diverse menu featurnig a delectable array of dishes.Our mission is to satisfy tour craving and elevate your dining experinece,
              one delicius meal at time.</p>
-        <div className="explre-menu-list">
+        <div className="explore-menu-list">
             {menu_list.map((item, index)=>{
                 return (
-                    <div key={index} className="explore-menu-item">
-                        <img src={item.menu_image} alt="" />
+                    <div onClick={()=>setCategory(prev =>prev===item.menu_name?"ALL":item.menu_name)} key={index} className="explore-menu-list-item">
+                        <img  className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
                         <p>{item.menu_name}</p>
                     </div>
                 )
             })}
         </div>
+        <hr />
     </div>
   )
 }
